165711494e61a2c0a2c3062075246eb6ddf03bc0
toantran2206 <toantran2206@gmail.com>
2021-09-22 12:04:02 +0700

    Update plan_b_app.py

--- plan_b_app.py
+++ plan_b_app.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title('This is a tile')
 
 st.header('Diver-conscinousness')
-write('Hi there! Welcome to our diver-consciousness app!')
+st.write('Hi there! Welcome to our diver-consciousness app!')
 
 values = st.slider(
     'Select a range of values',
