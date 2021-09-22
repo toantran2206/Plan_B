69833ac588dcc21d71fea4cab559fe0eb9e8b17f
toantran2206 <toantran2206@gmail.com>
2021-09-22 21:13:35 +0700

    Update plan_b_app.py

--- plan_b_app.py
+++ plan_b_app.py
@@ -10,8 +10,8 @@ user_input = st.text_input("Please tell us your name:")
 if user_input:
     st.write('Hi, ',user_input)
     st.write('This a diver-consciousness quiz with 40 questions. There is not right or wrong answer. You just need to think carefully and choose which suites you best!')
-    st.button("Click here to start")
-    values = st.slider(
-        'Select a range of values',
-        0.0, 100.0, (25.0, 75.0))
-    st.write('Values:', values)
+    if st.button("Click here to start"):
+        values = st.slider(
+            'Select a range of values',
+            0.0, 100.0, (25.0, 75.0))
+        st.write('Values:', values)
